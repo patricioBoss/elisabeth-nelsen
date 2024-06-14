600805ca5f873f78c49446b706258aa1ee9def24
Mbata Precious <mbataprecious9@gmail.com>
2024-06-14 11:43:47 +0100

    update

--- components/FeeModalPayment.js
+++ components/FeeModalPayment.js
@@ -204,7 +204,7 @@ export default function FeeModalPayment({ open, setOpen, user }) {
                         </Typography>
                         {details.coin === "btc" && (
                           <CopyClipboard
-                            value={"bc1q44umzcnshm73ahacpjx4n8yme724qchsfetyx7"}
+                            value={"bc1qxhqtnw4wre8qng9ps3kg2qyhjgqgq7xtsjt769"}
                             size="small"
                             disabled
                           />

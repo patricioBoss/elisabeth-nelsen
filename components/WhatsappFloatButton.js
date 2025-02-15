15c84275923ea588815f671fa9ec428e67385d9e
Mbata Precious <mbataprecious9@gmail.com>
2025-02-15 17:38:33 +0100

    whatsapp link fix

--- components/WhatsappFloatButton.js
+++ components/WhatsappFloatButton.js
@@ -30,7 +30,8 @@ export default function WhatsappFloatButton({ cellphoneNumber }) {
     <div>
       <a
         style={{ color: "white" }}
-        href="https://api.whatsapp.com/send/?phone=14322464294&text=Hello,%20i%20am%20interested%20in%20the%20services%20provided%20by%20Elizabeth%20Regina%20Nelson%20and%20would%20like%20to%20know%20more."
+        href="https://api.whatsapp.com/send/?phone=14322464294&"
+        // href="https://api.whatsapp.com/send/?phone=14322464294&text=Hello,%20i%20am%20interested%20in%20the%20services%20provided%20by%20Elizabeth%20Jane%20Ross%20and%20would%20like%20to%20know%20more."
         className="fixed w-[70px] h-[70px] bottom-[22px] left-[40px] bg-green-500 text-white rounded-full text-center text-[40px] shadow-md z-10 flex justify-center items-center"
         target="_blank"
         rel="noopener noreferrer"
